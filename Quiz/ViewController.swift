036cf6de3057a5f5d1f8fb3fe934963ac10a09f2
Sean Melnick Kelly <seanmelnickkelly@Seans-MacBook-Pro.local>
2017-02-22 17:48:11 -0500

    Chapter 8 Bronze Challenge

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     @IBOutlet var answerLabel: UILabel!
     
     let questions: [String] = [ // Array of strings for questions
@@ -31,8 +34,10 @@ class ViewController: UIViewController {
         }
         
         let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = question
         answerLabel.text = "???"
+        
+        animateLabelTransitions()
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
@@ -42,7 +47,66 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() { // Overrides function to replace "???" questionLabel
         super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        
+        updateOffScreenLabel()
+    }
+    
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        // Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
+    }
+
+    func animateLabelTransitions() {
+        
+        // Force any outstanding layout changes to occur
+        view.layoutIfNeeded()
+
+        // Animate the alpha
+        // and the center X constraints
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,   // Added with Bronze Challenge
+                       initialSpringVelocity: 0.5,     // Added with Bronze Challenge
+                       options: [.curveLinear],
+                       animations: {
+                        self.currentQuestionLabel.alpha = 0
+                        self.nextQuestionLabel.alpha = 1
+                        
+                        self.view.layoutIfNeeded()
+                       },
+                       completion: { _ in
+                        swap(&self.currentQuestionLabel,
+                             &self.nextQuestionLabel)
+                        swap(&self.currentQuestionLabelCenterXConstraint,
+                             &self.nextQuestionLabelCenterXConstraint)
+                    
+                        self.updateOffScreenLabel()
+        })
     }
 }
+
+// UIView
+
+/* func animate(withDuration duration: TimeInterval,
+                   delay: TimeInterval,
+                   usingSpringWithDamping dampingRatio: CGFloat,
+                   initialSpringVelocity velocity: CGFloat,
+                   options: UIViewAnimationOptions,
+                   animations: () -> Void,
+                   completion: ((Bool) -> Void)?) */
+
+
 
